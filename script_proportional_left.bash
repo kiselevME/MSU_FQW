7cee8ab7863b14a13cd96173ab3a612a2e5d6258
KiselevME <maximkiselev8376@gmail.com>
2025-04-20 12:24:09 +0300

    Add new config for proportional reinsurance case

--- script_proportional_left.bash
+++ script_proportional_left.bash
@@ -3,6 +3,6 @@ for n_simulations in 10 20 50 100
 do
 for max_time in 10 50 100 200 500 1000 5000
 do
-python main_proportional_left.py --config=./configs/proportional/config_1.json --n_simulations=$n_simulations --max_time=$max_time --seed_offset=0
+python main_proportional_left.py --config=./configs/proportional/config_2.json --n_simulations=$n_simulations --max_time=$max_time --seed_offset=0
 done
 done
--- script_proportional_right.bash
+++ script_proportional_right.bash
@@ -3,6 +3,6 @@ for n_simulations in 10 20 50 100 200 500 1000
 do
 for max_time in 10 50 100 200 500 1000 5000 10000 50000 100000
 do
-python main_proportional_right.py --config=./configs/proportional/config_1.json --n_simulations=$n_simulations --max_time=$max_time --seed_offset=100000
+python main_proportional_right.py --config=./configs/proportional/config_2.json --n_simulations=$n_simulations --max_time=$max_time --seed_offset=100000
 done
 done
